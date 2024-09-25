628de918195af30b6b00d8f41b5b5ab18f7163c2
adrian palomo faña <adrianpalomofe@gmail.com>
2024-09-25 14:06:28 +0200

    Programa añadiendo Edad y Año de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,6 @@
 public class Prueba {
-    public static void main(String args[]){
+    public static void main(String[] args) {
         System.out.println("Palomo Fana");
-        	    }
+        System.out.println("Tu Edad: 19, Nacido en: 2005");
+    }
 }
