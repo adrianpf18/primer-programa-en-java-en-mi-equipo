012a82740f69bc4cf3160b012e8115ac34e90cd0
adrian palomo faña <adrianpalomofe@gmail.com>
2024-09-25 14:08:33 +0200

    Programa añadiendo mi comida favorita

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba {
     public static void main(String[] args) {
         System.out.println("Palomo Fana");
         System.out.println("Tu Edad: 19, Nacido en: 2005");
+	System.out.println("Mi comida favorita: Tostones");
     }
 }
