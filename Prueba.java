f12119fbd267ebb5c27a17435dcf1a3e7dc194b1
adrian palomo faña <adrianpalomofe@gmail.com>
2024-10-01 09:21:30 +0200

    Eliminada la línea de apellidos y añadido mi cantante fav.

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba {
     public static void main(String[] args) {
-        System.out.println("Palomo Fana");
         System.out.println("Tu Edad: 19, Nacido en: 2005");
 	System.out.println("Mi comida favorita: Tostones");
+	System.out.println("Mi cantante favorito es: The Weekend");
     }
 }
